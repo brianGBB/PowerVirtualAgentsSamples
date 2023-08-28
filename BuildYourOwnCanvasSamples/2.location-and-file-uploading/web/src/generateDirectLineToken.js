6ea1673aa3691c6ba8b895827e22fc15518fcce4
Brian Gregor <brian.gregor@globalblackbelts.com>
2023-08-28 14:11:22 -0400

    adds config data; removes .env from gitignore; corrects directline url

--- BuildYourOwnCanvasSamples/2.location-and-file-uploading/web/src/generateDirectLineToken.js
+++ BuildYourOwnCanvasSamples/2.location-and-file-uploading/web/src/generateDirectLineToken.js
@@ -7,7 +7,7 @@ module.exports = async function generateDirectLineToken(botId, botTenantId) {
   
   // The URL host in which to generate the Direct Line token is subject to change based off of environment.
   // The host should be consistent to the host that you are building your Power VA.
-  const response = await fetch('https://ae785cc9623f408c927b0308045be3.72.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cr196_directLine/directline/token?api-version=2022-03-01-preview&botId=directlinetoken?botId=' + botId + '&tenantId=' + botTenantId, {method: "GET"});
+  const response = await fetch('https://7436d0405e8c4f2fbb6f672d449892.ea.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cr196_directLine/directline/token?api-version=2022-03-01-preview' + botId + '&tenantId=' + botTenantId, {method: "GET"});
 
   const { token } = await response.json();
 
